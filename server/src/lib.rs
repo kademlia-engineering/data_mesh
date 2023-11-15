c871cdaa417c909f9990c6bed85354f03316f885
jamesdean0507 <jamesdean0507@gmail.com>
2023-11-15 10:26:52 -0500

    Initialize web server

---
+++ server/src/lib.rs
@@ -0,0 +1,41 @@
+/*
+Last updated: 11-15-2023
+
+Description: This crate defines the web server class using tokio and hyper
+
+Author: James Dean
+*/
+use hyper::{Body, Request, Response, Server, StatusCode};
+use hyper::service::{make_service_fn, service_fn};
+use std::convert::Infallible;
+use std::net::SocketAddr;
+
+// This method defines the rest api routes exposed by the codebase
+// Params: HTTP Request
+// Returns: HTTP Response
+async fn handle_request(req: Request<Body>) -> Result<Response<Body>, Infallible> {
+    match req.uri().path() {
+        "/" => Ok(Response::new(Body::from("Welcome to the Rust Hyper Server!"))),
+        "/hello" => Ok(Response::new(Body::from("Hello, World!"))),
+        _ => {
+            let mut not_found = Response::default();
+            *not_found.status_mut() = StatusCode::NOT_FOUND;
+            Ok(not_found)
+        }
+    }
+}
+
+// This method initializes the web server
+// Params: Port
+pub async fn run_server(port: u16) {
+    let addr = SocketAddr::from(([0, 0, 0, 0], port));
+    let make_svc = make_service_fn(|_conn| async {
+        Ok::<_, Infallible>(service_fn(handle_request))
+    });
+
+    let server = Server::bind(&addr).serve(make_svc);
+
+    if let Err(e) = server.await {
+        eprintln!("server error: {}", e);
+    }
+}
