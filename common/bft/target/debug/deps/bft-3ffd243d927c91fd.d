73acd2b3321de2b0c7f6814a94ed528983b2f24f
jamesdean0507 <jamesdean0507@gmail.com>
2023-12-27 23:16:20 -0500

    update gitignore

--- common/bft/target/debug/deps/bft-3ffd243d927c91fd.d
+++
@@ -1,5 +0,0 @@
-/Users/amity/kademlia/data_mesh/common/bft/target/debug/deps/bft-3ffd243d927c91fd.rmeta: src/main.rs
-
-/Users/amity/kademlia/data_mesh/common/bft/target/debug/deps/bft-3ffd243d927c91fd.d: src/main.rs
-
-src/main.rs:
--- common/bft/target/debug/deps/bft-dfcd5585b16265c5.d
+++
@@ -1,5 +0,0 @@
-/Users/amity/kademlia/data_mesh/common/bft/target/debug/deps/bft-dfcd5585b16265c5.rmeta: src/main.rs
-
-/Users/amity/kademlia/data_mesh/common/bft/target/debug/deps/bft-dfcd5585b16265c5.d: src/main.rs
-
-src/main.rs:
